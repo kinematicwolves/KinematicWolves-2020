56edc35ac9b50e228c6ec467a43adc6b1c090799
Mike Dennis <mikeadennis@gmail.com>
2021-02-18 18:48:28 -0500

    Set motors to zero speed when intake command finished
    - Add call to move conveyors at output 0 in the driver command for the intake.

--- src/main/java/frc/robot/commands/RunIntake.java
+++ src/main/java/frc/robot/commands/RunIntake.java
@@ -43,7 +43,7 @@ public class RunIntake extends CommandBase {
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-
+    m_conveyorSubsystem.move_conveyors(0);
   }
 
   // Returns true when the command should end.
